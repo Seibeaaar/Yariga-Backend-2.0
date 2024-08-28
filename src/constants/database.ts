d20650e3a96b3a9fb0661af58bcf638470d561c7
Seibeaaar <yuribar4520@gmail.com>
2024-08-28 13:14:49 +0300

    Connect ot databas

---
+++ src/constants/database.ts
@@ -0,0 +1,1 @@
+export const MONGO_DB_CONNECT_URI = `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@yariga.ccndk.mongodb.net/?retryWrites=true&w=majority&appName=Yariga`;
